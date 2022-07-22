8e5dbfff177ffe0a26409499a35439e8d6032c96
Mak Alamin <mak@Mak-Alamin.localdomain>
2022-07-22 22:53:34 +0600

    added codes

--- DiagonalDifference.js
+++ DiagonalDifference.js
@@ -1,7 +1,57 @@
-const arr = [
-    [2,2,2],
-    [3,3,3],
-    [4,6,4],
-  ];
-  
-  console.log(arr[2][2]);
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+  console.log(inputString);
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+  // Write your code here
+  return arr;
+}
+
+function main() {
+  const ws = fs.createWriteStream("output.txt");
+
+  const n = parseInt(readLine().trim(), 10);
+
+  let arr = Array(n);
+
+  for (let i = 0; i < n; i++) {
+    arr[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((arrTemp) => parseInt(arrTemp, 10));
+  }
+
+  const result = diagonalDifference(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
